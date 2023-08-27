f64515daeda42d0a07216e7ef67cd64b76120b6d
gustavo <gustavo.cesariosilva15@gmail.com>
2023-08-27 14:22:16 -0300

    Adicionando projeot ao github

---
+++ src/main/java/br/com/caelum/vraptor/controller/PacienteController.java
@@ -0,0 +1,15 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+
+@Path("paciente")
+@Controller
+public class PacienteController {
+	
+	@Get("")
+	public void paciente() {
+		
+	}
+}
