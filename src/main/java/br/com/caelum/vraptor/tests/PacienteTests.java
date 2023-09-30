165c0cd5321dddbe0ed2537760f1658b1e9b4e9c
gustavo <gustavo.cesariosilva15@gmail.com>
2023-09-30 17:02:43 -0300

    Feat: Tela de Pacientes

--- src/main/java/br/com/caelum/vraptor/tests/PacienteTests.java
+++ src/main/java/br/com/caelum/vraptor/tests/PacienteTests.java
@@ -1,33 +1,8 @@
 package br.com.caelum.vraptor.tests;
 
-import java.util.List;
-
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import br.com.caelum.vraptor.connection.ConnectionFactory;
-import br.com.caelum.vraptor.dao.DAO;
-import br.com.caelum.vraptor.dao.PacienteDAO;
-import br.com.caelum.vraptor.dao.PacienteTestDAO;
-import br.com.caelum.vraptor.model.Paciente;
-
 public class PacienteTests {
-	
-	
-	
 	
 	public static void main(String[] args) {
 		
-		PacienteTestDAO dao = new PacienteTestDAO();
-		Paciente p = new Paciente();
-		p.setNome("Gustavo D");
-		p.setComorbidade("Bronquite");
-		dao.save(p);
-	
-	
-	
 	}
-	
 }
